34a457745aff4e3b78cf58502b8521d0e3b4d09b
Luciano Rizky Pratama <88131579+TooRizky@users.noreply.github.com>
2022-05-08 23:33:34 +0700

    Update counting_Sort.c

--- Praktikum 9/counting_Sort.c
+++ Praktikum 9/counting_Sort.c
@@ -16,7 +16,7 @@ int main(){
 	void printArray(int array[], int LengthArray);   /*deklarasi prosedur printArray*/
 
 /*Algoritma*/
-	printf("Program Counting Sort \n");
+	printf("Counting Sort Program \n");
     printf("Banyaknya bilangan yang ingin diurutkan = ");
     scanf("%d", &N);
 	
